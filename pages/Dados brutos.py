b1800d48218b8c7407f5a07587047c15ad69b66f
SergioEryCazella <sergio.cazella@gmail.com>
2025-11-11 19:19:47 -0300

    Create Dados brutos.py

---
+++ pages/Dados brutos.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import requests
+import pandas as pd
+
+st.title('DADOS BRUTOS')
+
+url = 'https://labdados.com/produtos'
+
+response = requests.get(url)
+dados = pd.DataFrame.from_dict(response.json())
+dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format='%d/%m/%Y')
+
+with st.expander('Colunas'):
+    colunas = st.multiselect('Selecione as colunas', list(dados.columns), list(dados.columns))
+
+st.sidebar.title('Filtros')
+with st.sidebar.expander('Nome do Produto'):
+    produtos = st.multiselect('Selecione os produtos', dados['Produto'].unique(), dados['Produto'].unique())
+with st.sidebar.expander('Categoria do Produto'):
+    categoria = st.multiselect('Selecione as Categorias', dados['Categoria do Produto'].unique(), dados['Categoria do Produto'].unique())
+with st.sidebar.expander('Preço do Produto'):
+    preco = st.slider('Selecione o preço', 0, 5000, (0,5000))
+with st.sidebar.expander('Data da Compra'):
+    data_compra = st.date_input('Selecione a data', (dados['Data da Compra'].min(), dados['Data da Compra'].max()))    
+with st.sidebar.expander('Local da Compra'):
+    local_compra = st.multiselect('Selecione o Local', dados['Local da compra'].unique(), dados['Local da compra'].unique())
+
+query = '''
+Produto in @produtos and \
+`Categoria do Produto` in @categoria and \
+@preco[0] <= Preço <= @preco[1] and \
+@data_compra[0] <= `Data da Compra` <= @data_compra[1] and \
+`Local da compra` in @local_compra
+'''
+dados_filtrados = dados.query(query)
+dados_filtrados = dados_filtrados[colunas]
+
+st.dataframe(dados_filtrados)
+
+st.markdown(f'A tabela possui :blue[{dados_filtrados.shape[0]}] linhas e :blue[{dados_filtrados.shape[1]}] colunas')
